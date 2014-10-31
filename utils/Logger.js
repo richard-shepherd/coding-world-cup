7eb9c4581194cdba6939a1417d92fff49c53d2f7
richard <richard.s.shepherd@gmail.com>
2014-10-31 20:33:12 +0000

    cwc

---
+++ utils/Logger.js
@@ -0,0 +1,22 @@
+/**
+ * Logger
+ * ------
+ * This "logger" is really a log-broker.
+ *
+ * You register logging classes with it, which each implement
+ * a log(message) method. When you log to this class, it forwards
+ * the message to the registered brokers.
+ *
+ * This class manages log levels, formatting and indenting as well
+ * as the collection of loggers.
+ */
+
+/**
+ * Constructor.
+ */
+Logger = new function() {
+}
+
+
+
+

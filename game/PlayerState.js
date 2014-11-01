834e734d054dee92fdea634b47ccf8fa62ff1eb3
richard <richard.s.shepherd@gmail.com>
2014-11-01 09:11:08 +0000

    cwc

---
+++ game/PlayerState.js
@@ -0,0 +1,24 @@
+/**
+ * PlayerState
+ * -----------
+ * Information about one player.
+ *
+ * This includes dynamic data such as the player's current position
+ * and speed, as well as more static / config data such as the player's
+ * skills and abilities.
+ *
+ * Most, but not all, of the data is serialized and passed to AIs
+ * each turn of the game. Some data - in particular the player's
+ * "intentions" are private, and are only available to the AI which
+ * is controlling the player.
+ */
+
+/**
+ * @constructor
+ */
+function PlayerState() {
+
+    //
+}
+
+

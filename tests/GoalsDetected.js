5c00c97cb233561c455950ed1ac9947fdf723a21
richard <richard.s.shepherd@gmail.com>
2014-11-28 07:05:09 +0000

    cwc

--- tests/GoalsDetected.js
+++ tests/GoalsDetected.js
@@ -8,7 +8,9 @@ var TestUtilsLib = require('../test_utils');
 var MockGame_CalculationInterval = TestUtilsLib.MockGame_CalculationInterval;
 var GameLib = require('../game');
 var TeamState = GameLib.TeamState;
-
+var Pitch = GameLib.Pitch;
+var UtilsLib = require('../utils');
+var Utils = UtilsLib.Utils;
 
 /**
  * Straight bounce left
@@ -51,5 +53,40 @@ exports['Straight bounce left'] = function(test) {
     test.done();
 };
 
+/**
+ * Diagonal right
+ * --------------
+ * The ball travels at an angle into the right goal.
+ */
+exports['Diagonal right'] = function(test) {
+    // We set up the game...
+    var game = new MockGame_CalculationInterval(1.0);
+    game._aiUpdateIntervalSeconds = 1.0;
+
+    // We set the team directions...
+    var team1 = game.getTeam1();
+    team1.setDirection(TeamState.Direction.RIGHT);
+    var team2 = game.getTeam2();
+    team2.setDirection(TeamState.Direction.LEFT);
+
+    // We put the ball near the left-hand goal, heading towards it...
+    var ball = game.ball;
+    var ballState = ball.state;
+    var ballPosition = ballState.position;
+    ball.friction = 0.0;
+    ballPosition.x = Pitch.WIDTH -  4.0;
+    ballPosition.y = Pitch.GOAL_Y2 + 3;
+    ballState.vector = Utils.vectorFromDirection(45);
+    ballState.speed = 10.0;
+
+    // We calculate...
+    game.calculate();
+
+    // Team1 should have scored...
+    test.equal(team1.state.score, 1);
+    test.equal(team2.state.score, 0);
+
+    test.done();
+};
 
 
